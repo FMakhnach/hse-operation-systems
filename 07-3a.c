72bd550e181afeafdb5c66c6c11e9ce24a179fdc
Fedor Makhnach <fomakhnach@edu.hse.ru>
2021-04-12 14:15:55 +0300

    Lab 9 done.

---
+++ 07-3a.c
@@ -0,0 +1,107 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+void try_semop(int sem_id, int sem_op) {
+  struct sembuf mybuf;
+  mybuf.sem_num = 0;
+  mybuf.sem_op  = sem_op;
+  mybuf.sem_flg = 0;
+
+  if (semop(sem_id, &mybuf, 1) < 0) {
+    printf("Can\'t wait for condition\n");
+    exit(-1);
+  }
+}
+
+void P(int sem_id) {
+  try_semop(sem_id, -1);
+}
+
+void V(int sem_id) {
+  try_semop(sem_id, 1);
+}
+
+int create_sem() {
+  int    semid;
+  char   pathname[]="07-3a.c";
+  key_t  key;
+
+  if ((key = ftok(pathname,0)) < 0) {
+    printf("Can\'t generate key\n");
+    exit(-1);
+  }
+  if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+    printf("Can\'t create semaphore set\n");
+    exit(-1);
+  }
+  return semid;
+}
+
+int main()
+{
+  int     *array;
+  int     shmid;
+  int     new = 1;
+  char    pathname[] = "07-3a.c";
+  key_t   key;
+  int     sem_id;
+
+  if ((key = ftok(pathname,0)) < 0) {
+    printf("Can\'t generate key\n");
+    exit(-1);
+  }
+
+  if ((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0) {
+    if (errno != EEXIST) {
+      printf("Can\'t create shared memory\n");
+      exit(-1);
+    } else {
+      if ((shmid = shmget(key, 3*sizeof(int), 0)) < 0) {
+        printf("Can\'t find shared memory\n");
+        exit(-1);
+      }
+      new = 0;
+    }
+  }
+
+  if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
+    printf("Can't attach shared memory\n");
+    exit(-1);
+  }
+
+  // Create or get semaphore.
+  sem_id = create_sem();
+  if(new){
+    // Increasing semaphore value by 1.
+    V(sem_id);
+  }
+
+  if (new) {
+    P(sem_id);
+    array[0] =  1;
+    array[1] =  0;
+    array[2] =  1;
+    V(sem_id);
+  } else {
+    P(sem_id);
+    array[0] += 1;
+    array[2] += 1;
+    V(sem_id);
+  }
+
+  printf
+    ("Program 1 was spawn %d times, program 2 - %d times, total - %d times\n",
+    array[0], array[1], array[2]);
+
+  if (shmdt(array) < 0) {
+    printf("Can't detach shared memory\n");
+    exit(-1);
+  }
+
+  return 0;
+}
